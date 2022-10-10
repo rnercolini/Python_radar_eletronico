393a0a3d9bbdfc5867741e2afe9444fa1b00feef
rnercolini <106276254+rnercolini@users.noreply.github.com>
2022-10-10 15:49:49 -0300

    Delete Desafio29.py

--- Desafio29.py
+++
@@ -1,5 +0,0 @@
-v = float(input('Digite a velocidade do veículo: '))
-if v > 80:
-    print('A sua velocidade é de {} km/h e você ultrapassou a velocidade permitida.'.format(v))
-    print('A sua multa será no valor de R${:.2f}.'.format((v-80)*7))
-print('Dirija com segurança!')

5584b963ac7db4d43652212503eba3f8d5faa18a
rnercolini <106276254+rnercolini@users.noreply.github.com>
2022-10-10 15:47:58 -0300

    Add files via upload

---
+++ Desafio29.py
@@ -0,0 +1,5 @@
+v = float(input('Digite a velocidade do veículo: '))
+if v > 80:
+    print('A sua velocidade é de {} km/h e você ultrapassou a velocidade permitida.'.format(v))
+    print('A sua multa será no valor de R${:.2f}.'.format((v-80)*7))
+print('Dirija com segurança!')
